41d49ac74cfd9aaa7c2fb4da1182b8ec6d150596
James Bithell <Jbithell@users.noreply.github.com>
2016-02-24 18:17:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var monitors = [
-    "apikey1",  //Place your first Key Here
-    "apikey2", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
+    "m776982558-1efb178a25c1f6312b8bfe41",  //TellyStorm
+    "m777124032-35bbc5a34084420f96eeebb0", //iMUN
 
     
 ];
-var title = 'My Project Status Page'; //Page <title> and heading at top of page
+var title = 'BluePost Status'; //Page <title> and heading at top of page
 var refreshtime = 5; //How often to refresh in seconds
-var footermessage = ''; //HTML to go in footer
+var footermessage = 'See <a href="//bluepost.xyz">BluePost.xyz</a> for more information'; //HTML to go in footer
